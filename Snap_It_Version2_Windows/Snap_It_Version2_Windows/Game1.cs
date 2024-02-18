a26bb9c18e6975c38fd910bce0442a2688fd6303
victoriaccmak <vmak@uwaterloo.ca>
2024-02-18 08:49:18 -0500

    Updated the user interface

--- Snap_It_Version2_Windows/Snap_It_Version2_Windows/Game1.cs
+++ Snap_It_Version2_Windows/Snap_It_Version2_Windows/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
 
 namespace Snap_It_Version2_Windows
 {
@@ -12,12 +13,21 @@ namespace Snap_It_Version2_Windows
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public const int SCREEN_WIDTH = 430 * 2 / 3;
+        public const int SCREEN_WIDTH = 430 * 2 / 3; 
         public const int SCREEN_HEIGHT = 932 * 2 / 3;
 
         public const int LOGO_STATE = 0;
         public const int PIN_STATE = 1;
         public const int HOME_STATE = 2;
+        public const int ANALYTICS_STATE = 3;
+        public const int CAMERA_STATE = 4;
+        public const int ANALYZED_STATE = 5;
+        public const int SETTINGS = 6;
+        public const int TRACKING_STATE = 7;
+        public const int JOURNAL = 8;
+        public const int COMMUNITY = 9;
+
+        public const int MENU_BTN_SPACING = 30;
 
         private string password = "1234";
 
@@ -39,6 +49,11 @@ namespace Snap_It_Version2_Windows
         private Texture2D bar;
         private Texture2D dropdown;
 
+        private Texture2D camIcon;
+
+        private Texture2D profilePic;
+        private Texture2D profileMask;
+
         private Rectangle[] numBtnRecs = new Rectangle[10];
         private Rectangle arrowUpRec;
         private Rectangle arrowDownRec;
@@ -53,13 +68,20 @@ namespace Snap_It_Version2_Windows
         private Rectangle barRec;
         private Rectangle dropdownRec;
 
+        private Rectangle camIconRec;
+
         private int gameState = LOGO_STATE;
 
         private SpriteFont font;
 
+        private SoundEffect clickSnd;
+
         private string pinMsg = "Enter your passcode";
         private Vector2 pinMsgLoc;
         private Rectangle [] pinLocs = new Rectangle[4];
+        
+        private Rectangle profilePicRec;
+        private Rectangle profileMaskRec;
 
         private MouseState prevMouse;
         private MouseState mouse;
@@ -114,7 +136,7 @@ namespace Snap_It_Version2_Windows
             numBtns[9] = Content.Load<Texture2D>("Imgs/Btns/Btn9");
 
             arrowDown = Content.Load<Texture2D>("Imgs/Btns/ArrowDown");
-            arrowUp = Content.Load<Texture2D>("Imgs/Btns/ArrowDown");
+            arrowUp = Content.Load<Texture2D>("Imgs/Btns/ArrowUp");
             arrowLeft = Content.Load<Texture2D>("Imgs/Btns/ArrowBack");
             arrowRight = Content.Load<Texture2D>("Imgs/Btns/ArrowGo");
 
@@ -126,6 +148,7 @@ namespace Snap_It_Version2_Windows
 
             menuBtn = Content.Load<Texture2D>("Imgs/Btns/menuBtn");
             logo = Content.Load<Texture2D>("Imgs/Btns/SnapIt200Pxl");
+            camIcon = Content.Load<Texture2D>("Imgs/Btns/camIcon");
 
             pixel = Content.Load<Texture2D>("Imgs/Btns/PixelImg");
 
@@ -133,7 +156,12 @@ namespace Snap_It_Version2_Windows
             bar = Content.Load<Texture2D>("Imgs/BgImgs/SnapItBar");
             dropdown = Content.Load<Texture2D>("Imgs/BgImgs/SnapItDropdown");
 
+            profilePic = Content.Load<Texture2D>("Imgs/Btns/profilePic");
+            profileMask = Content.Load<Texture2D>("Imgs/Btns/profilePicMask");
+
             font = Content.Load<SpriteFont>("Fonts/MenuFont");
+
+            clickSnd = Content.Load<SoundEffect>("sounds/BtnSelectSnd");
 
             numBtnRecs[1] = new Rectangle(SCREEN_WIDTH / 3 - numBtns[0].Width, SCREEN_HEIGHT * 2 / 3, numBtns[0].Width, numBtns[0].Height);
             numBtnRecs[2] = new Rectangle(2 + numBtnRecs[1].Right, SCREEN_HEIGHT * 2 / 3, numBtns[0].Width, numBtns[0].Height);
@@ -154,21 +182,29 @@ namespace Snap_It_Version2_Windows
             pinLocs[0] = new Rectangle(pinLocs[1].X - 20, (int)pinMsgLoc.Y + 50, 5, 5);
             pinLocs[3] = new Rectangle(pinLocs[2].X + 20, (int)pinMsgLoc.Y + 50, 5, 5);
 
+            motiv1Rec = new Rectangle(0, 0, SCREEN_WIDTH, motiv1.Height);
+            barRec = new Rectangle(0, 0, SCREEN_WIDTH, bar.Height);
+            dropdownRec = new Rectangle(0, 0, SCREEN_WIDTH, dropdown.Height);
+            //arrowDownRec = new Rectangle(SCREEN_WIDTH / 2 - arrowDown.Width / 2, barRec.Bottom - arrowDown.Height - 5, arrowDown.Width, arrowDown.Height);
+            arrowUpRec = new Rectangle(SCREEN_WIDTH / 2 - arrowDown.Width / 2, dropdownRec.Bottom - arrowDown.Height - 5, arrowDown.Width, arrowDown.Height);
+            menuBtnRec = new Rectangle(10, barRec.Bottom - menuBtn.Height - 10, menuBtn.Width, menuBtn.Height);
 
             //arrowDownRec = Content.Load<Texture2D>("Imgs/Btns/ArrowDown");
             //arrowUpRec = Content.Load<Texture2D>("Imgs/Btns/ArrowDown");
             //arrowLeftRec = Content.Load<Texture2D>("Imgs/Btns/ArrowBack");
             //arrowRightRec = Content.Load<Texture2D>("Imgs/Btns/ArrowGo");
 
-            //optionBtnRecs[0] = Content.Load<Texture2D>("Imgs/Btns/OptionBtn1");
-            //optionBtnRecs[1] = Content.Load<Texture2D>("Imgs/Btns/OptionBtn2");
-            //optionBtnRecs[2] = Content.Load<Texture2D>("Imgs/Btns/OptionBtn3");
-            //optionBtnRecs[3] = Content.Load<Texture2D>("Imgs/Btns/OptionBtn4");
-            //optionBtnRecs[4] = Content.Load<Texture2D>("Imgs/Btns/OptionBtn5");
+            optionBtnRecs[0] = new Rectangle(SCREEN_WIDTH / 2 - optionBtns[0].Width / 2, barRec.Bottom, optionBtns[0].Width, optionBtns[0].Height);
+            optionBtnRecs[1] = new Rectangle(SCREEN_WIDTH / 2 - optionBtns[0].Width / 2, optionBtnRecs[0].Bottom + MENU_BTN_SPACING, optionBtns[0].Width, optionBtns[0].Height);
+            optionBtnRecs[2] = new Rectangle(SCREEN_WIDTH / 2 - optionBtns[0].Width / 2, optionBtnRecs[1].Bottom + MENU_BTN_SPACING, optionBtns[0].Width, optionBtns[0].Height);
+            optionBtnRecs[3] = new Rectangle(SCREEN_WIDTH / 2 - optionBtns[0].Width / 2, optionBtnRecs[2].Bottom + MENU_BTN_SPACING, optionBtns[0].Width, optionBtns[0].Height);
+            optionBtnRecs[4] = new Rectangle(SCREEN_WIDTH / 2 - optionBtns[0].Width / 2, optionBtnRecs[3].Bottom + MENU_BTN_SPACING, optionBtns[0].Width, optionBtns[0].Height);
 
-            //menuBtnRec = Content.Load<Texture2D>("Imgs/Btns/menuBtn");
+            camIconRec = new Rectangle(SCREEN_WIDTH / 2 - camIcon.Width / 2, SCREEN_HEIGHT - 80, camIcon.Width, camIcon.Height);
+
             logoRec = new Rectangle(SCREEN_WIDTH / 2 - logo.Width / 2, SCREEN_HEIGHT / 2 - logo.Height / 2, logo.Width, logo.Height);
 
+            //profilePicRec = new Rectangle(barRec.Bottom + profileMask.Width, profileMask.Height);
             //motiv1Rec = Content.Load<Texture2D>("Imgs/BgImgs/DailyMotivation1");
             //barRec = Content.Load<Texture2D>("Imgs/BgImgs/SnapItBar");
             //dropdownRec = Content.Load<Texture2D>("Imgs/BgImgs/SnapItDropdown");
@@ -212,7 +248,7 @@ namespace Snap_It_Version2_Windows
                     {
                         for (int i = 0; i < 10; i++)
                         {
-                            if (mouse.LeftButton == ButtonState.Pressed && prevMouse.LeftButton != ButtonState.Pressed && numBtnRecs[i].Contains(mouse.Position))
+                            if (BtnPressed(numBtnRecs[i]))
                             {
                                 numNumsEntered++;
                                 curPassword += "" + i;
@@ -229,9 +265,59 @@ namespace Snap_It_Version2_Windows
                         curPassword = "";
                         numNumsEntered = 0;
                     }
-
                     break;
                 case HOME_STATE:
+                    if (droppedDown)
+                    {
+                        if (BtnPressed(arrowUpRec))
+                        {
+                            droppedDown = false;
+                        }
+                        else if (BtnPressed(optionBtnRecs[0]))
+                        {
+                            gameState = ANALYTICS_STATE;
+                        }
+                        else if (BtnPressed(optionBtnRecs[1]))
+                        {
+                            gameState = TRACKING_STATE;
+                        }
+                        else if (BtnPressed(optionBtnRecs[2]))
+                        {
+                            gameState = JOURNAL;
+                        }
+                        else if (BtnPressed(optionBtnRecs[3]))
+                        {
+                            gameState = COMMUNITY;
+                        }
+                        else if (BtnPressed(optionBtnRecs[4]))
+                        {
+                            gameState = SETTINGS;
+                        }
+                    }
+                    else
+                    {
+                        if (BtnPressed(menuBtnRec))
+                        {
+                            droppedDown = true;
+                        }
+                    }
+
+                    if (BtnPressed(camIconRec))
+                    {
+                        gameState = CAMERA_STATE;
+                    }
+                    break;
+                case CAMERA_STATE:
+                    break;
+                case ANALYTICS_STATE:
+                    break;
+                case TRACKING_STATE:
+                    break;
+                case JOURNAL:
+                    break;
+                case COMMUNITY:
+                    break;
+                case SETTINGS:
                     break;
                 default:
                     break;
@@ -240,6 +326,7 @@ namespace Snap_It_Version2_Windows
             base.Update(gameTime);
         }
 
+        
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -254,10 +341,11 @@ namespace Snap_It_Version2_Windows
             switch (gameState)
             {
                 case LOGO_STATE:
+                    GraphicsDevice.Clear(Color.White);
                     spriteBatch.Draw(logo, logoRec, Color.White);
                     break;
                 case PIN_STATE:
-
+                    GraphicsDevice.Clear(Color.White);
                     spriteBatch.DrawString(font, pinMsg, pinMsgLoc, Color.Black);
                     
                     for (int i = 0; i < numNumsEntered; i++)
@@ -273,7 +361,41 @@ namespace Snap_It_Version2_Windows
                     }
                     break;
                 case HOME_STATE:
-                    //spriteBatch.Draw()
+                    GraphicsDevice.Clear(Color.CornflowerBlue);
+                    
+                    if (droppedDown)
+                    {
+                        spriteBatch.Draw(motiv1, motiv1Rec, Color.White * 0.5f);
+                        spriteBatch.Draw(dropdown, dropdownRec, Color.White);
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            spriteBatch.Draw(optionBtns[i], optionBtnRecs[i], Color.White);
+                        }
+
+                        spriteBatch.Draw(arrowUp, arrowUpRec, Color.White);
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(motiv1, motiv1Rec, Color.White);
+                        spriteBatch.Draw(bar, barRec, Color.White);
+                        spriteBatch.Draw(menuBtn, menuBtnRec, Color.White);
+                    }
+
+                    spriteBatch.Draw(camIcon, camIconRec, Color.White);
+
+                    break;
+                case CAMERA_STATE:
+                    break;
+                case ANALYTICS_STATE:
+                    break;
+                case TRACKING_STATE:
+                    break;
+                case JOURNAL:
+                    break;
+                case COMMUNITY:
+                    break;
+                case SETTINGS:
                     break;
                 default:
                     break;
@@ -282,5 +404,18 @@ namespace Snap_It_Version2_Windows
             spriteBatch.End();
             base.Draw(gameTime);
         }
+
+        private bool BtnPressed(Rectangle btnRec)
+        {
+            if (mouse.LeftButton == ButtonState.Pressed && prevMouse.LeftButton != ButtonState.Pressed && btnRec.Contains(mouse.Position))
+            {
+                clickSnd.CreateInstance().Play();
+                return true;
+            }
+
+            return false;
+        }
     }
 }
+
+
